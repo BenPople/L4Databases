79ab4806ea0a949ef3e0473daaae6c27fa4798d6
Ben Pople <56734659+BenPople@users.noreply.github.com>
2023-03-21 10:47:09 +0000

    Create CreateDB.sql

---
+++ CreateDB.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE library;
+
+USE library;
+
+CREATE TABLE Author (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE CoAuthor (
+    coauthor_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_id INT NOT NULL,
+    book_id INT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES Author(author_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    author_id INT NOT NULL,
+    genre VARCHAR(50),
+    publication_year INT,
+    FOREIGN KEY (author_id) REFERENCES Author(author_id)
+);
+
+CREATE TABLE LoanedBook (
+    loanedbook_id INT PRIMARY KEY AUTO_INCREMENT,
+    borrower_id INT NOT NULL,
+    book_id INT NOT NULL,
+    FOREIGN KEY (borrower_id) REFERENCES Borrowers(borrower_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
+CREATE TABLE Loans (
+    loan_id INT PRIMARY KEY AUTO_INCREMENT,
+    loanedbook_id INT NOT NULL,
+    loan_date DATE NOT NULL,
+    return_date DATE,
+    FOREIGN KEY (loanedbook_id) REFERENCES LoanedBook(loanedbook_id)
+);
+
+CREATE TABLE Borrowers (
+    borrower_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
